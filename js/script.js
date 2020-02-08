f052ce9081ead6285b991fac7012be3d6b25ec21
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2020-02-08 15:06:37 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//release date
+var releaseDate = new Date('Dec 30, 2020 00:00:00');
+
+//check for the interval
+function interval(){
+	//get current time
+	var currentDate = new Date().getTime();
+	
+	var duration = releaseDate - currentDate;
+}
+interval();
