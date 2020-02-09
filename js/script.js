137835f203d47c2119baeba2413767feafcf8dce
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2020-02-09 23:46:05 +0600

    update

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var releaseDate = new Date('Dec 30, 2020 00:00:00');
 var countdownSelector = document.querySelector("#countdown");
 var counterNumber = document.getElementsByClassName("counter-number");
 var counterBox = document.getElementsByClassName("counter-box");
+const value = 60 * 60 * 1000;
 window.onload = interval;
 //check for the interval
 var interval = setInterval(interval, 1000);
@@ -11,9 +12,9 @@ function interval(){
 	//get current time
 	var currentDate = new Date().getTime();
 	var duration = releaseDate - currentDate;
-	var days 	= Math.floor(duration / (24 * 60 * 60 * 1000));
-	var hours 	= Math.floor(duration % (24 * 60 * 60 * 1000) / (60 * 60 * 1000));
-	var mins 	= Math.floor(duration % (60 * 60 * 1000) / (60 * 1000));
+	var days 	= Math.floor(duration / (24 * value));
+	var hours 	= Math.floor(duration % (24 * value) / (value));
+	var mins 	= Math.floor(duration % (value) / (60 * 1000));
 	var seconds = Math.floor(duration % (60 * 1000) /  1000);
 	var arr = [days, hours, mins, seconds];
 	for(var i = 0; i < counterNumber.length; i++){
